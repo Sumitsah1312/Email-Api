982ce49488f02e9560642791863bbe725dda6f0d
sumit shah <sumit.shah@mashvirtual.in>
2025-10-31 15:25:58 +0530

    cc

--- api/server.js
+++ api/server.js
@@ -6,9 +6,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Mail API is running...");
-});
 
 app.post("/PortfolioConnectMail", async (req, res) => {
   try {
